933c6c143b85b57360ca6935210c0f04953582f0
Crystal <muthonigachanja2000@gmail.com>
2023-06-15 09:05:16 -0700

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px")
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = dodger.style.left;
+    left = parseInt(left) || 0; // Use 0 as default value
+
+    if (left < 360) {
+        const newPosition = left + 10;
+        dodger.style.left = `${newPosition}px`;
+    }
+}
